41acaa0d7fc8264f7887013c927d2752297b8018
Saophalkun Ponlu <phalkunz@gmail.com>
2017-09-01 10:31:34 +1200

    FIX crop campaign item header text

--- client/src/containers/CampaignAdmin/CampaignAdminItem.js
+++ client/src/containers/CampaignAdmin/CampaignAdminItem.js
@@ -104,7 +104,7 @@ class CampaignAdminItem extends SilverStripeComponent {
       <div className="fill-width">
         {thumbnail}
         <div className="list-group-item__details">
-          <h4 className="list-group-item__heading">{item.Title}</h4>
+          <h4 className="list-group-item__heading" title={item.Title}>{item.Title}</h4>
           {links}
           {badge.className && badge.Title &&
             <span className={badge.className}>{badge.Title}</span>
