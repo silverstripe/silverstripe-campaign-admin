b2dd5e64bf7a1af571f2f50f66614456c2e3ab15
Damian Mooyman <damian@silverstripe.com>
2017-10-10 17:05:54 +1300

    Update usages of deprecated API

--- src/CampaignAdminExtension.php
+++ src/CampaignAdminExtension.php
@@ -3,16 +3,14 @@
 namespace SilverStripe\CampaignAdmin;
 
 use SilverStripe\Core\Extension;
-use SilverStripe\Core\Manifest\ModuleLoader;
 use SilverStripe\View\Requirements;
 
 class CampaignAdminExtension extends Extension
 {
     public function init()
     {
-        $module = ModuleLoader::getModule('silverstripe/campaign-admin');
-        Requirements::add_i18n_javascript($module->getRelativeResourcePath('client/lang'), false, true);
-        Requirements::javascript($module->getRelativeResourcePath("client/dist/js/bundle.js"));
-        Requirements::css($module->getRelativeResourcePath("client/dist/styles/bundle.css"));
+        Requirements::add_i18n_javascript('silverstripe/campaign-admin: client/lang', false, true);
+        Requirements::javascript('silverstripe/campaign-admin: client/dist/js/bundle.js');
+        Requirements::css('silverstripe/campaign-admin: client/dist/styles/bundle.css');
     }
 }
