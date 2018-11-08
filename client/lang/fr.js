6ff950943734a40689b2d14d299fdc9e12b14d07
Aaron Carlino <unclecheese@leftandmain.com>
2018-11-08 16:25:42 +1300

    Update translations

--- client/lang/fr.js
+++ client/lang/fr.js
@@ -7,10 +7,10 @@ if (typeof(ss) === 'undefined' || typeof(ss.i18n) === 'undefined') {
 } else {
   ss.i18n.addDictionary('fr', {
     "CampaignAdmin.ADDCAMPAIGN": "Ajouter une campagne",
-    "CampaignAdmin.DELETECAMPAIGN": "Etes-vous sûr de vouloir supprimer cet enregistrement ?",
+    "CampaignAdmin.DELETECAMPAIGN": "Êtes-vous sûr de vouloir supprimer cet enregistrement ?",
     "CampaignAdmin.DRAFT": "Brouillon",
     "CampaignAdmin.ITEM_SUMMARY_PLURAL": "%s éléments",
-    "CampaignAdmin.ITEM_SUMMARY_SINGULAR": "%s éléments",
+    "CampaignAdmin.ITEM_SUMMARY_SINGULAR": "%s élément",
     "CampaignAdmin.MODIFIED": "Modifié",
     "CampaignAdmin.NO_CHANGES": "Aucun changement",
     "CampaignAdmin.PUBLISHCAMPAIGN": "Publier la campagne",
