3e2e8996859fe880eb772e9ace13d03b3f432d96
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2024-08-21 12:14:18 +1200

    API Deprecate extension to be replaced with config

--- src/CampaignAdminExtension.php
+++ src/CampaignAdminExtension.php
@@ -4,13 +4,23 @@ namespace SilverStripe\CampaignAdmin;
 
 use SilverStripe\Admin\LeftAndMain;
 use SilverStripe\Core\Extension;
+use SilverStripe\Dev\Deprecation;
 use SilverStripe\View\Requirements;
 
 /**
  * @extends Extension<LeftAndMain>
+ * @deprecated 5.3.0 Will be replaced with YAML configuration
  */
 class CampaignAdminExtension extends Extension
 {
+    public function __construct()
+    {
+        Deprecation::withNoReplacement(
+            fn () => Deprecation::notice('5.3.0', 'Will be replaced with YAML configuration', Deprecation::SCOPE_CLASS)
+        );
+        parent::__construct();
+    }
+
     public function init()
     {
         Requirements::add_i18n_javascript('silverstripe/campaign-admin: client/lang', false);
