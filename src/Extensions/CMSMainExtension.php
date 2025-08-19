a65060d57a4ff96334b932a8a3e89d89e984fb01
Guy Sartorelli <36352093+GuySartorelli@users.noreply.github.com>
2025-08-19 16:57:24 +1200

    FIX Generate correct archive message (#367)
    CMSMain doesn't include the record in the descendants array anymore, but
    we do need to know if the current record has a campaign associated with
    it or not.

--- src/Extensions/CMSMainExtension.php
+++ src/Extensions/CMSMainExtension.php
@@ -6,11 +6,16 @@ use SilverStripe\Core\Extension;
 use SilverStripe\Versioned\ChangeSet;
 use SilverStripe\Versioned\ChangeSetItem;
 use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\ORM\DataObject;
 
 class CMSMainExtension extends Extension
 {
     function updateArchiveWarningMessage(string &$message, array $descendants)
     {
+        /** @var DataObject $record */
+        $record = func_get_arg(2);
+        // Get all changesets including for the current record
+        $descendants[] = $record->ID;
         $inChangeSetIDs = ChangeSetItem::get()->filter([
             'ObjectID' => $descendants,
             'ObjectClass' => SiteTree::class
@@ -26,7 +31,7 @@ class CMSMainExtension extends Extension
         }
         $numCampaigns = ChangeSet::singleton()->i18n_pluralise($affectedChangeSetCount);
         $numCampaigns = mb_strtolower($numCampaigns ?? '');
-        if ($affectedChangeSetCount > 0) {
+        if (count($descendants) > 1) {
             $message = _t(
                 __CLASS__ . '.ArchiveWarningWithChildrenAndCampaigns',
                 'Warning: This page and all of its child pages will be unpublished and automatically removed from'
